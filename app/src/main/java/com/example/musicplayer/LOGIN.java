bd8e0c3b5627af3e7c53f90399a3486228fd6ad4
Youssefanter <51537980+Youssefanter@users.noreply.github.com>
2020-09-22 17:49:12 +0200

    Project with SQLite

--- app/src/main/java/com/example/musicplayer/LOGIN.java
+++ app/src/main/java/com/example/musicplayer/LOGIN.java
@@ -7,10 +7,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LOGIN extends AppCompatActivity {
-    private Button button;
-
+    private Button submitL;
+    private EditText UserN;
+    private EditText UserP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +22,33 @@ public class LOGIN extends AppCompatActivity {
 
 
 
-        button=(Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        submitL=(Button)findViewById(R.id.button);
+        UserN=(EditText)findViewById(R.id.name);
+        UserP=(EditText)findViewById(R.id.pass);
+        final AvalancheDB ExsistUser=new AvalancheDB(this);
+
+
+        submitL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                configNextButton();
+                if (ExsistUser.VerifyUser(UserN.getText().toString(),UserP.getText().toString()))
+                {
+                    Toast.makeText(getApplicationContext(),"Logged in successfully :)",Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(LOGIN.this , Home_Page.class));
+                }
+                else{
+                    
+                    Toast.makeText(getApplicationContext(),"SomeThing went Wrong !",Toast.LENGTH_LONG).show();
+                }
+                
+
             }
 
         });
-        Button b=(Button)findViewById(R.id.buttonreg);
-        b.setOnClickListener(new View.OnClickListener() {
+
+
+        Button reg=(Button)findViewById(R.id.buttonreg);
+        reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 RegisterPage();
@@ -43,8 +63,7 @@ public class LOGIN extends AppCompatActivity {
     }
 
     private void configNextButton(){
-        Button nextbutton = (Button) findViewById(R.id.button);
-        nextbutton.setOnClickListener(new View.OnClickListener() {
+        submitL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LOGIN.this , Home_Page.class));
